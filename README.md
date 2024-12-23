28d7a439565bcc4dd8c11e3a91490dc38f6134e6
arong <xurongdong@hotmail.com>
2024-12-23 14:56:13 +0800

    update theme

--- README.md
+++ README.md
@@ -4,8 +4,12 @@ A theme for Hugo.
 
 ## Features
 
-* support for dark mode
-* responsive design
+* 简约设计(Less is more)
+    - 无广告
+    - 干净的页面, 尤其是博文内容上, 专注于内容, 禁止多余的元素, 甚至可以缺少一些功能(比如导航, 用浏览器自带的就好)
+* 支持黑暗模式
+* 响应式设计(支持电脑, 平板, 手机)
+* 支持代码主题
 
 
 ## Installation
@@ -15,3 +19,14 @@ git submodule add https://github.com/arong/hugo-theme-aronica.git
 ```
 
 ## Configuration
+
+## TODO
+
+1. 修复主题切换bug
+1. 适配手机端
+1. 为导航栏增加悬停效果, 激活效果
+1. 为代码块增加复制按钮
+1. light模式下的代码块样式
+1. 设计一个logo
+1. 设计一个favicon
+1. 设计首页
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['content/**/*.md', 'layouts/**/*.html', "assets/js/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            a: {
+              textDecoration: 'underline',
+              textDecorationColor: theme('colors.gray.500'),
+              textUnderlineOffset: '4px', // 全局下划线偏移
+            },
+          },
+        },
+      }),
+    },
   },
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
