6d2fc9ba2ce066c88362c922144f5757beb8fd89
arong <xurongdong@hotmail.com>
2025-03-22 19:55:27 +0800

    update package depency

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ module.exports = {
       }),
     },
   },
-  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  plugins: {"@tailwindcss/postcss": {}},
   daisyui: {
     themes: ["dark", "light"],
   },
